fd7756a771912a02c8ad30039ea67e405e073886
uday <udaysubba2004@gmail.com>
2025-07-12 15:32:25 +0530

    feat: create Home page and update route to render Home component

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <Head title="Home">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <AppLayout>
+        Home Page
+    </AppLayout>
+</template>
