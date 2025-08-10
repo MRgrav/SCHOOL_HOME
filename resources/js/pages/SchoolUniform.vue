d6a631c8dfc351bd88b17a9ea44201fb44536f94
uday <udaysubba2004@gmail.com>
2025-08-10 13:51:36 +0530

    fix: nav bar overlay in school uniform page

--- resources/js/pages/SchoolUniform.vue
+++ resources/js/pages/SchoolUniform.vue
@@ -11,6 +11,7 @@ console.log(pdf);
 </script>
 <template>
     <AppLayout>
+        <div class="my-3"></div>
         <VuePDF v-for="pageNo in 10" :pdf="pdf" fit-parent :page="pageNo" :key="pageNo"/>
     </AppLayout>
 </template>
